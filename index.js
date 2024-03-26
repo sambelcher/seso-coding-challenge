f7770e683c6f4ce91a89d02f904b17a06bcd7878
Sam Belcher <belche27@msu.edu>
2024-03-25 19:26:01 -0700

    In async solution, pop multiple logs each time - issue is that calling promse.resolve() on two+ popAsync() promiseswill return the exact same log date + message for each promise.

--- index.js
+++ index.js
@@ -4,38 +4,38 @@ const LogSource = require("./lib/log-source");
 const Printer = require("./lib/printer");
 
 function runSolutions(sourceCount) {
-  return new Promise((resolve, reject) => {
-    /**
-     * Challenge Number 1!
-     *
-     * Assume that a LogSource only has one method: pop() which will return a LogEntry.
-     *
-     * A LogEntry is simply an object of the form:
-     * {
-     * 		date: Date,
-     * 		msg: String,
-     * }
-     *
-     * All LogEntries from a given LogSource are guaranteed to be popped in chronological order.
-     * Eventually a LogSource will end and return boolean false.
-     *
-     * Your job is simple: print the sorted merge of all LogEntries across `n` LogSources.
-     *
-     * Call `printer.print(logEntry)` to print each entry of the merged output as they are ready.
-     * This function will ensure that what you print is in fact in chronological order.
-     * Call 'printer.done()' at the end to get a few stats on your solution!
-     */
-    const syncLogSources = [];
-    for (let i = 0; i < sourceCount; i++) {
-      syncLogSources.push(new LogSource());
-    }
-    try {
-      require("./solution/sync-sorted-merge")(syncLogSources, new Printer());
-      resolve();
-    } catch (e) {
-      reject(e);
-    }
-  }).then(() => {
+  // return new Promise((resolve, reject) => {
+  //   /**
+  //    * Challenge Number 1!
+  //    *
+  //    * Assume that a LogSource only has one method: pop() which will return a LogEntry.
+  //    *
+  //    * A LogEntry is simply an object of the form:
+  //    * {
+  //    * 		date: Date,
+  //    * 		msg: String,
+  //    * }
+  //    *
+  //    * All LogEntries from a given LogSource are guaranteed to be popped in chronological order.
+  //    * Eventually a LogSource will end and return boolean false.
+  //    *
+  //    * Your job is simple: print the sorted merge of all LogEntries across `n` LogSources.
+  //    *
+  //    * Call `printer.print(logEntry)` to print each entry of the merged output as they are ready.
+  //    * This function will ensure that what you print is in fact in chronological order.
+  //    * Call 'printer.done()' at the end to get a few stats on your solution!
+  //    */
+  //   const syncLogSources = [];
+  //   for (let i = 0; i < sourceCount; i++) {
+  //     syncLogSources.push(new LogSource());
+  //   }
+  //   try {
+  //     require("./solution/sync-sorted-merge")(syncLogSources, new Printer());
+  //     resolve();
+  //   } catch (e) {
+  //     reject(e);
+  //   }
+  // }).then(() => {
     return new Promise((resolve, reject) => {
       /**
        * Challenge Number 2!
@@ -58,8 +58,8 @@ function runSolutions(sourceCount) {
         .then(resolve)
         .catch(reject);
     });
-  });
+  // });
 }
 
 // Adjust this input to see how your solutions perform under various loads.
-runSolutions(100);
+runSolutions(10);
--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -7,28 +7,28 @@ const {
 // Print all entries, across all of the *async* sources, in chronological order.
 
 /**
- * Similar concept to the sync-sorted-merge
- * During initial stack creation we can do each promise at the same time, we don't need to wait for the previous one to resolve
- * When processing each part of the logSources, we always have to wait for the next log entry to resolve
- * otherwise we have no way of telling what the next value is, and if it will be > or < than
- * Brainstorming ways to make this more efficient
- *
  * Bottleneck - waiting for each individual log to resolve from popAsync every time.
  *
- * IDEA: can we be more efficient by processing two values at once all the time?
- * for example, if we process two entries at once for a logSource - when we go
- * to print the log, we can immediately check if the next one is still in order, or if we should return to the stack
- *
- * There is still a bottleneck here if we do process two entries in a row. How do we avoid this bottleneck?
- * IDEA V2: Can we start processing all of the entries in every logSource during our inital stack creation?
- * - during initial stack creation, try to start resolving every promise of every logEntry in every logSource
- * - Once our stack has 1 value from each logSource we start processing the stack?
+ * for example, if we process N (we can adjust value of N to control whether we care more about time or space) entries at once for a logSource - when we go
+ * to print the nextLog, we don't always have to wait for another popAsync, if there is still anothe entry from that logSource in our queue
+ * - pop first log from each source at same time - this could be increased potentially as well to grab more than the first log.
+ * - each time we run out of logs from a logSource in the queue, we pop N logs
+ * - the next N times we check that logSource, we can just pull directly from the priority queue instead
+ * - use a separate array to store counts to tell if we should grab more logs from the logSource
+ *    - array index corresponds to index in logSources, and value is # of logs from this logsource in the priority queue
+ *    - if counts[currentLogSource] <= 0, pop off N more logs, otherwise grab from the priorityqueue
  */
+
+// this constant can be adjusted to any number we want, depending on if we are prioritizing space or time
+// higher limit => faster processing, but potentially more space used since more entries may be stored in the priority queue
+// We could use logSources.length instead, to ensure we process results faster, as the input size gets bigger? This would also increase space used as input size got bigger though.
+const NUMBER_OF_LOGS_TO_PROCESS_AT_ONCE = 2;
 
 module.exports = async (logSources, printer) => {
 
   // initialize priority queue with date as the comparator
   const recentLogsQueue = new MinPriorityQueue(logAndIndex => logAndIndex.log.date);
+  const logsInQueueForLogSource = Array(logSources.length).fill(0);
 
   // get first log from each logSource
   const promises = logSources.map((logSource) => {
@@ -38,6 +38,7 @@ module.exports = async (logSources, printer) => {
   // resolve all of the first logs at same time
   await Promise.all(promises).then((data) => {
     data.forEach((log, idx) => {
+      logsInQueueForLogSource[idx] += 1;
       recentLogsQueue.enqueue({ logSourceIndex: idx, log });
     });
   });
@@ -45,17 +46,28 @@ module.exports = async (logSources, printer) => {
   // Process next log in queue, and replace with next log from that logSource
   while (!recentLogsQueue.isEmpty()) {
     const logAndIndex = recentLogsQueue.dequeue();
+    logsInQueueForLogSource[logAndIndex.logSourceIndex] -= 1;
     printer.print(logAndIndex.log)
 
-    // get next log from logSource, and add to queue if not drained
-    const logSource = logSources[logAndIndex.logSourceIndex];
-    const nextLog = await logSource.popAsync();
+    // only need to pop more logs if this logSource has 0 logs in the queue
+    if (logsInQueueForLogSource[logAndIndex.logSourceIndex] <= 0) {
+      // get next log from logSource, and add to queue if not drained
+      const logSource = logSources[logAndIndex.logSourceIndex];
+      const promises = []
 
-    // check if logSource is drained
-    if (!nextLog) {
-      continue
-    } else {
-      recentLogsQueue.enqueue({ logSourceIndex: logAndIndex.logSourceIndex, log: nextLog })
+      for (let i = 0; i < NUMBER_OF_LOGS_TO_PROCESS_AT_ONCE; i++) {
+        promises.push(logSource.popAsync());
+      }
+
+      // resolve all of the first logs at same time
+      await Promise.all(promises).then((data) => {
+        data.forEach((log) => {
+          if (log) {
+            logsInQueueForLogSource[logAndIndex.logSourceIndex] += 1;
+            recentLogsQueue.enqueue({ logSourceIndex: logAndIndex.logSourceIndex, log });
+          }
+        });
+      });
     }
   }
 
